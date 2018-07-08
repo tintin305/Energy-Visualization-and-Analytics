c3691baceb10b22e050854837d9e8e19d84949fa
Tristan Kuisis <tristankuisis1@gmail.com>
2018-07-08 17:44:45 +0200

    Updated scraping file to loop through all of the dates. A number of fixes for errors (commented in code). Added *.png to gitignore

--- Web Scraping/Downloading Data/test.py
+++ Web Scraping/Downloading Data/test.py
@@ -21,48 +21,59 @@ def editorLooper(i, j):
 
     # Formatting the dates
     startDate = str(i) + "-" + str(j) + "-" + "01" + " 00:00"
-    startDate = str(i) + "-" + str(j+1) + "-" + "01" + " 00:00"
+    endDate = str(i) + "-" + str(j+1) + "-" + "01" + " 00:00"
     
     dateSelectorStart.send_keys(Keys.CONTROL, "a")
-    dateSelectorStart.send_keys("2013-01-01 00:00")
+    dateSelectorStart.send_keys(startDate)
+    dateSelectorStart.send_keys(Keys.ENTER)
     dateSelectorEnd.send_keys(Keys.CONTROL, "a")
-    dateSelectorEnd.send_keys("2013-02-01 00:00")
+    dateSelectorEnd.send_keys(endDate)
     dateSelectorEnd.send_keys(Keys.ENTER)
 
     driver.get_screenshot_as_file("date.png")
 
     # Select the required channel
     channelIterator = range(0,99)
-    for test in channelIterator:
-        channelSelectorCheckBox = driver.find_element_by_id('ctl00_ContentPlaceHolder_Body_ctrlEntitySelector_grdEntities_DXSelBtn' + str(test) + '_D')
-        channelSelectorCheckBox.click()
+    for checkBox in channelIterator:
+        channelSelectorCheckBox = driver.find_element_by_id('ctl00_ContentPlaceHolder_Body_ctrlEntitySelector_grdEntities_DXSelBtn' + str(checkBox) + '_D')
+        # To check whether the selected checkbox is already checked
+        isTickedText = channelSelectorCheckBox.get_attribute("class")
+        isCheckedTest = "dxWeb_edtCheckBoxChecked_DevEx dxICheckBox_DevEx dxichSys"
+        if isTickedText != isCheckedTest:
+            channelSelectorCheckBox.click()
+
 
     # Click the view button
     viewButton = driver.find_element_by_id('ContentPlaceHolder_Body_btnView')
+    driver.get_screenshot_as_file("channelselectorbtn.png")
     viewButton.click()
-
     driver.get_screenshot_as_file("view.png")
 
     # Click the export button
+    # This sleep is required such that the error: "Other element would receive click" does not happen
+    sleep(15)
     exportButton = driver.find_element_by_id('ContentPlaceHolder_Body_btnExportData')
     exportButton.click()
 
     driver.get_screenshot_as_file("export.png")
 
-    sleep(2)
+    # sleep(2)
 
-    driver.quit()
-    return;
+    # driver.quit()
+    return
 
 url = "https://www.ecwin.co.za/ecWIN/wits/Login"
 
 chrome_options = Options()
 # Run Chrome Headless
+
 # chrome_options.add_argument("--headless")
 # chrome_options.add_argument("--window-size=1920x1080")
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.implicitly_wait(30)
+driver.maximize_window()
 driver.get(url)
+
 
 # User Details
 usernameField = driver.find_element_by_id('ContentPlaceHolder_Body_txtUsername')
@@ -93,3 +104,5 @@ day = 1
 for i in year:
     for j in month:
         editorLooper(i, j)
+
+driver.quit()
