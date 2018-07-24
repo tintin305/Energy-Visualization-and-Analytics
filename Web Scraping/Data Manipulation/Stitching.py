6da3a424a699873f2bad0d9b74a7b97e585371dd
MLHeimann <heimannmarion@gmail.com>
2018-07-24 09:24:56 +0200

    Refactoring of stitching.py

--- Web Scraping/Data Manipulation/Stitching.py
+++ Web Scraping/Data Manipulation/Stitching.py
@@ -7,60 +7,68 @@ import errno
 import datetime
 import sys
 
-# Directory for where to find the separated csv files
-rootSeparatedFolder = 'C:\Project\Data\Separated'
+def makeDirectoryFolder(newDir):
+    try:
+        os.makedirs(newDir)
+    except OSError:
+        if not os.path.isdir(newDir):
+            raise    
+    return
 
-# Create the folder for the stitched together documents (if it doesn't already exist)
-rootStitchingFolder = 'C:\Project\Data\Stitched'
-try:
-    os.makedirs(rootStitchingFolder)
-except OSError:
-    if not os.path.isdir(rootStitchingFolder):
-        raise    
+def changeToInputDirectory(inDir):
+    try:
+        os.chdir(inDir) # Change to seperated directory
+    except:
+        sys.exit("Folder with input data does not exist") # Exit program if input data folder is not found
+    return
 
-# Makes a list of all the folders in the cwd
-folderNames = []
-
-try:
-    os.chdir(rootSeparatedFolder) # Change to seperated directory
-except:
-    sys.exit("Folder with seperated data does not exist") # Exit program if input data folder is not found
-
-folderNames = os.listdir()
-
-# Loop through all of the folders, this will work so that the work is done in each of the folders
-for folders in folderNames:
-    loggerFolder = str(rootSeparatedFolder) + '\\' + str(folders)
-    os.chdir(loggerFolder)
-
-    # Get a list for the files in the cwd
+def listOfCSV():
     allFiles = os.listdir()
     files = []
     for csvFiles in allFiles:
         if csvFiles.endswith(".csv"):
             files.append(csvFiles)
-    # Now do the operations on the files
+    return files
 
-    mergedFrames = pd.DataFrame()
+def stitchChannelData(folders):
+    loggerFolder = str(rootSeparatedFolder) + '\\' + str(folders)
+    changeToInputDirectory(loggerFolder)
+
+    # Get a list for the files in the cwd
+    # Now do the operations on the files
+    files = listOfCSV()
+
     list_ = []
     for file_ in files:
         df = pd.read_csv(file_, index_col=None, header=0)
         list_.append(df)
     frame = pd.concat(list_)
-
-    fileName = frame.columns.values[1]
-    dataLoggerFolder = str(rootStitchingFolder) + '\\' + str(fileName)
-
-    try:
-        os.makedirs(dataLoggerFolder)
-    except OSError:
-        if not os.path.isdir(dataLoggerFolder):
-            raise    
-
-    os.chdir(dataLoggerFolder)
-    frame.to_csv(fileName+'.csv', sep=",", encoding='utf-8', index=False)
-    
-    
     
         # At this stage, it may be useful to convert the date to a date time object, or a UNIX goody
         # One can do this: http://pbpython.com/excel-file-combine.html
+    fileName = frame.columns.values[1]
+    dataLoggerFolder = str(rootStitchingFolder) + '\\' + str(fileName)
+    makeDirectoryFolder(dataLoggerFolder)
+    os.chdir(dataLoggerFolder)
+    frame.to_csv(fileName+'.csv', sep=",", encoding='utf-8', index=False)
+    return
+###########################################################
+#                               Main
+###########################################################
+
+# Define variables:
+# Directory for where to find the separated csv files
+rootSeparatedFolder = 'C:\Project\Data\Separated'
+
+# Create the folder for the stitched together documents (if it doesn't already exist)
+rootStitchingFolder = 'C:\Project\Data\Stitched'
+makeDirectoryFolder(rootStitchingFolder)
+changeToInputDirectory(rootSeparatedFolder)
+folderNames = os.listdir()
+# Loop through all of the folders, this will work so that the work is done in each of the folders
+for folders in folderNames:
+    stitchChannelData(folders)
+    
+    
+    
+    
