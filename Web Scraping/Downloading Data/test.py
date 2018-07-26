6ec1591c6a5f20a1c1c37f99c8782ada23d3b1f4
Tristan Kuisis <tristankuisis1@gmail.com>
2018-07-26 21:00:51 +0200

    rebase test2

--- Web Scraping/Downloading Data/test.py
+++ Web Scraping/Downloading Data/test.py
@@ -37,7 +37,7 @@ def dateSelector(year, isFirstHalf):
         startDate = str(year) + "-" + str(7) + "-" + "01" + " 00:00"
         endDate = str(year+1) + "-" + str(1) + "-" + "02" + " 00:00"
 
-    
+    dateSelectorStart.click()
     WebDriverWait(driver, 10).until(expected_conditions.visibility_of(dateSelectorStart)) 
     dateSelectorStart.send_keys(Keys.CONTROL, "a")
     dateSelectorStart.send_keys(startDate)
@@ -222,6 +222,7 @@ currentYear = datetime.datetime.now().year
 yearRange = range(2013, currentYear+1)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 nextChannelSet()
 nextChannelSet()
 nextChannelSet()
@@ -249,6 +250,9 @@ for checkBoxRange in range(23, totNrRanges+1):
     channelSelector(channelIter) # selects the checkboxes in the given range
 =======
 ChannelsToDownload = range(550, 623+1) # Download for channels 550 to 623
+=======
+ChannelsToDownload = range(570, 623+1) # Download for channels 550 to 623
+>>>>>>> Click date selector before entering date
 pageNr = 1
 # nextChannelSet()
 # nextChannelSet()
