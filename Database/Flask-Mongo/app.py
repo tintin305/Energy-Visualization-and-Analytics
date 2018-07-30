3e7e9fb94550fc233f053ab7751fd061aef063e3
Tristan Kuisis <tristankuisis1@gmail.com>
2018-07-30 08:17:32 +0200

    Basic Flask server which uses Dygraphs

--- Database/Flask-Mongo/app.py
+++ Database/Flask-Mongo/app.py
@@ -3,9 +3,9 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
 def index():
-    return render_template("LayoutTest.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host='127.0.0.1',port=5000,debug=True)
