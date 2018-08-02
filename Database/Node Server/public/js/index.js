961b42519e181293e9aa1836f8b49763e495b80e
MLHeimann <heimannmarion@gmail.com>
2018-08-02 08:43:48 +0200

    pull and rebase. Check that index.js still works

--- Database/Node Server/public/js/index.js
+++ Database/Node Server/public/js/index.js
@@ -1,23 +1,5 @@
-// g = new Dygraph(
-//     document.getElementById("graph"),
-//     // For possible data formats, see http://dygraphs.com/data.html
-//     // The x-values could also be dates, e.g. "2012/03/15"
-//     `X,Y,Z
-//     1,0,3
-//     2,2,6
-//     3,4,8
-//     4,6,9
-//     5,8,9
-//     6,10,8
-//     7,12,6
-//     8,14,3`,
-//     {
-//       // options go here. See http://dygraphs.com/options.html
-//       legend: 'always',
-//       animatedZooms: true,
-//       title: 'dygraphs chart template'
-//     });
 
+<<<<<<< HEAD
 g = new Dygraph(
 
   // containing div
@@ -40,3 +22,17 @@ g = new Dygraph(
 
 
 );
+=======
+  g = new Dygraph(
+
+    // containing div
+    document.getElementById("graphdiv"),
+  
+    // CSV or path to a CSV file.
+    "/Data/WITS_The_Junction_MSS_3_Kiosk_Sarie_Marais_kVarh.csv",
+    {
+    }    // Options
+  
+  
+  );
+>>>>>>> Removed csv from repo, deleted useless code
