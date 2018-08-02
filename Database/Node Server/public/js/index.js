3ffad60b59d9fa67c0929265ed1f1ea5bd9a56b6
MLHeimann <heimannmarion@gmail.com>
2018-08-02 16:14:21 +0200

    updated .gitignore and failed attempts for graphing unix timestamp

--- Database/Node Server/public/js/index.js
+++ Database/Node Server/public/js/index.js
@@ -1,15 +1,34 @@
-
-
-);
   g = new Dygraph(
 
     // containing div
     document.getElementById("graphdiv"),
   
     // CSV or path to a CSV file.
-    "/Data/WITS_The_Junction_MSS_3_Kiosk_Sarie_Marais_kVarh.csv",
-    {
-    }    // Options
-  
-  
+    "/tmp/temp.csv",
+    // {
+      // axis : {
+      //   x : {
+      //     valueFormatter: Dygraph.dateString_,
+      //     valueParser: function(x) { return 1000*parseInt(x); },
+      //     ticker: Dygraph.dateTicker                
+      //   }
+      // }
+    // }
+  {
+    // xRangePad: 10,
+    // yRangePad: 10,
+    // xValueFormatter: Dygraph.dateString_,
+    // xValueParser: function (x){
+    //   return 1000 * parseInt(x, 10);
+    // },
+    // xTicker: Dygraph.dateTicker,
+    // labels: ["Dates", "Not Kept", "Hosts"],
+    // title: "Promises not kept",
+    // legend: "always",
+    // drawPoints: "true",
+    // pointSize: 2,
+    // colors: ["orange", "blue", "black"],
+    // strokeWidth: 1
+ 
+  }
   );
