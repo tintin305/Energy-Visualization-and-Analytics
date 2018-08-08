020965ca9bc972d4f791914cbfbff4860960557e
Tristan Kuisis <tristankuisis1@gmail.com>
2018-08-08 08:42:00 +0200

    Rebase fixing

---
+++ Database/Node Server/public/js/Dygraphs/dygraphSettings.js
@@ -0,0 +1,12 @@
+g = new Dygraph(
+    document.getElementById("graphdiv"),
+        "/tmp/temp.csv",
+        {
+            axes: {
+            x : {
+            valueFormatter: Dygraph.dateString_,
+            ticker: Dygraph.dateTicker
+            }
+            },
+        }
+);
