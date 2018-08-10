a84d96c85261a9eecf5a6387120008d6a56e44ab
Tristan Kuisis <tristankuisis1@gmail.com>
2018-08-10 12:26:57 +0200

    Simple querying system such that opentsdb-node package is no longer required (to do: convert old request method to new python one)

---
+++ Database/Node Server/public/Python_Scripts/DatabaseQuery/DatabaseQuery.py
@@ -0,0 +1,98 @@
+import numpy as np
+import pandas as pd
+import json
+import requests
+import sys
+import os
+from collections import namedtuple
+import csv
+#  /usr/share/opentsdb/bin/tsdb query 1y-go  sum LoggerName
+
+#Read data from stdin
+def read_in():
+    lines = sys.stdin.readlines()
+    # Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines[0])
+
+def saveQueryDetails(queryDetails):
+    csvPath = os.path.join(os.path.dirname(__file__),"../../tmp/")
+    os.chdir(csvPath)
+    with open('queryDetails.txt','w') as write_file:
+        json.dump(queryDetails, write_file)
+
+def createQueryUrl(queryDetails):
+    # Get the details out of the object
+    aggregator = str(queryDetails['aggregator'])
+    downsample = queryDetails['downsample']
+    rate = queryDetails['metric']
+    tagKey = queryDetails['tagKey']
+    tagValue = queryDetails['tagValue']
+    metric = queryDetails['metric']
+    host = queryDetails['host']
+    port = queryDetails['port']
+    ms = queryDetails['ms']
+    arrays = queryDetails['arrays']
+    tsuids = queryDetails['tsuids']
+    annotations = queryDetails['annotations']
+    startDate = queryDetails['startDate']
+    endDate = queryDetails['endDate']
+
+    url = 'http://' + str(host) + ':' + str(port) + '/api/query?' + 'ms=' + ms + '&arrays=' + arrays + '&show_tsuids=' + tsuids + '&global_annotations=' + annotations + '&start=' + startDate + '&end=' + endDate + '&m=' + aggregator + ':' + downsample + ':' + metric + '{' + tagKey + '=' + tagValue + '}'
+   
+    csvPath = os.path.join(os.path.dirname(__file__),"../../tmp/")
+    os.chdir(csvPath)
+    f = open('url.txt','w')
+    f.write(url)
+    f.close()
+
+    return url
+
+def queryDatabase(url):
+    data = requests.get(url)
+    test = data.text
+    return test
+
+def writeDataToCSV(queryData):
+    csvPath = os.path.join(os.path.dirname(__file__),"../../tmp/")
+    os.chdir(csvPath)
+    with open('pythonData.csv','w') as write_file:
+        json.dump(queryData, write_file)    
+
+
+def extractData(queryData):
+    queryData = json.loads(queryData[1:-1])
+    dataArray = queryData['dps']
+
+    header = 'Timestamp,' +  str(queryData['metric'] + '\n')
+
+    data = str(dataArray)[1:-1]
+    data = data.strip('[')
+    data = data.replace('], [', '\n')
+    data = data.strip(']')
+
+    csvPath = os.path.join(os.path.dirname(__file__),"../../tmp/")
+    os.chdir(csvPath)
+    f = open('queryData.csv','w')
+    f.write(header)
+    f.write(data)
+    f.close()
+
+def main():
+    #get our data as an array from read_in()
+    queryDetails = read_in()
+    # Save query details to a text file (used for testing)
+    saveQueryDetails(queryDetails)
+
+    url = createQueryUrl(queryDetails)
+
+    queryData = queryDatabase(url)
+
+    writeDataToCSV(queryData)
+
+    extractData(queryData)
+
+    print(queryDetails)
+
+# Start process
+if __name__ == '__main__':
+    main()
--- Database/Node Server/server.js
+++ Database/Node Server/server.js
@@ -15,10 +15,8 @@ var jsdom = require('jsdom');
 // To export  to csv
 var fs = require('fs');
 
-
 //Running Python
 var PythonShell = require('python-shell');
-
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -128,65 +126,38 @@ app.get('/DataOutages', function(req, res){
 });
 
 app.get('/SankeyDiagram', function(req, res){
-    mQuery.aggregator('avg');
-    mQuery.downsample('5d-avg');
-    mQuery.rate(false);
-    mQuery.metric('WITS_EC_Matrix_Main_Incomer_kWh');
-    mQuery.tags('DataLoggerName', 'WITS_EC_Matrix_Main_Incomer_kWh');
-    client.host('localhost');
-    client.port(4242);
-    client.ms(false);
-    client.tsuids(false);
-    client.annotations('none');
-
-    // Defining the start date
-    client.start('2018/01/01 00:00');
-
-    // Defining the end date
-    client.end('2018/01/07 23:30');
-
-    client.arrays(false);
-    client.queries(mQuery);
-    var url = client.url();
-    console.log(url)
-    client.get(function onData(error, data){
-        if (error){
-            console.error( JSON.stringify(error));
-            return;
-        }
-        console.log(JSON.stringify(data))
-        // OGstring = JSON.stringify(data)
-        // dataString = OGstring.replace("]", " ");
-        // newstring = dataString;
-        // newstring = newstring.replace(/\],\[/g, '\n');
-        // dataIndex = newstring.indexOf('[[')
-        // newstring = newstring.substring(dataIndex+2)
-
-        // //  Remove unwanted brackets 
-        // newstring = newstring.replace(/\]/g, '');
-        // newstring = newstring.replace(/\}/g, '');
-        // // newstring = newstring.replace(/000,/g, ',');
-
-        // // NB //
-        // // Test if this works properly
-        // newstring = "Timestamp, " + req.params.DataloggerName   + "\n" + newstring;
-
-
-        // var dir = './public/tmp';
-        //     if (!fs.existsSync(dir)){
-        //         fs.mkdirSync(dir);
-        //     }
-        // fs.writeFile("./public/tmp/temp.csv", newstring, function(err) {
-        // if(err) {
-        //     return console.log(err);
-        // }
-        // console.log("The file was saved!");
-
-        }); 
-        res.sendFile(__dirname + '/Views/SankeyDiagram.html');
+    const spawn = require('child_process').spawn;
+    const scriptExecution = spawn("python.exe", [(__dirname +"/public/Python_Scripts/DatabaseQuery/DatabaseQuery.py")]);
+    
+    // Handle normal output
+    scriptExecution.stdout.on('data', (data) => {
+        console.log(String.fromCharCode.apply(null, data));
     });
+    
+    var options = {
+        aggregator: 'avg',
+        downsample: '2d-sum',
+        rate: 'false',
+        metric: 'WITS_EC_Matrix_Main_Incomer_kWh',
+        tagKey: 'DataLoggerName',
+        tagValue: 'WITS_EC_Matrix_Main_Incomer_kWh',
+        host: 'localhost',
+        port: 4242,
+        ms: 'false',
+        arrays: 'true',
+        tsuids: 'false',
+        annotations: 'none',
+        startDate: '2018/01/01-00:00',
+        endDate: '2018/01/07-23:30'
+    };
 
-// });
+    // Write data (remember to send only strings or numbers, otherwhise python wont understand)
+    var data = JSON.stringify(options);
+    scriptExecution.stdin.write(data);
+    // End data write
+    scriptExecution.stdin.end();
+    res.sendFile(__dirname + '/Views/SankeyDiagram.html');
+});
 
 // When the user enters 'localhost:3000/metrics/' then the server will query the database and return a list of metrics to the log and to the web page.
 app.get('/metrics/', function(req, res){
