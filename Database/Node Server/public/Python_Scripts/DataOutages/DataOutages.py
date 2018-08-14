53df1670171745ad394db7ea9ff84146198807d5
MLHeimann <heimannmarion@gmail.com>
2018-08-14 15:12:51 +0200

    Added headings for the graphs

--- Database/Node Server/public/Python_Scripts/DataOutages/DataOutages.py
+++ Database/Node Server/public/Python_Scripts/DataOutages/DataOutages.py
@@ -35,13 +35,13 @@ except:
     print("error loading csv")
     sys.exit()
 
-data_raw['dates'] = data_raw.Timestamp.apply(calculate_dates)
-data_raw['times'] = data_raw.Timestamp.apply(calculate_times)
-newdata = data_raw.drop('Timestamp', 1)
+data_raw['Date'] = data_raw.Timestamp.apply(calculate_dates)
+data_raw['Time of day'] = data_raw.Timestamp.apply(calculate_times)
+# newdata = data_raw.drop('Timestamp', 1)
 
-newdata["times"] = pd.Categorical(data_raw["times"], data_raw.times.unique())
+# newdata["times"] = pd.Categorical(data_raw["times"], data_raw.times.unique())
 
-datamatrix = data_raw.pivot("times", "dates", data_raw.columns.values[1])
+datamatrix = data_raw.pivot("Time of day", "Date", data_raw.columns.values[1])
 
 # Set two colours for the heatmap
 cmap =  mpl.colors.ListedColormap(['#000000','#ffffff'])
@@ -52,6 +52,7 @@ fig.set_size_inches(11.7, 8.27)
 sns.heatmap(datamatrix, xticklabels=50, cmap=cmap,vmax=0, cbar=False)
 plt.subplots_adjust(bottom=0.23, right=1, top=0.88)
 
+ax.set_title(data_raw.columns.values[1])
 directory = "../../Data/DataOutages"
 try:
         if not os.path.exists(directory):
