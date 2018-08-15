190ca61549e6166939063e880a2813b237b5da65
Tristan Kuisis <tristankuisis1@gmail.com>
2018-08-15 14:59:40 +0200

    Got Dygraphs querying working.

--- Database/Flask/static/Python_Scripts/DatabaseQueryDygraphs/DatabaseQueryDygraphs.py
+++ Database/Flask/static/Python_Scripts/DatabaseQueryDygraphs/DatabaseQueryDygraphs.py
@@ -8,56 +8,59 @@ from collections import namedtuple
 import csv
 #  /usr/share/opentsdb/bin/tsdb query 1y-go  sum LoggerName
 
-#Read data from stdin
-def read_in():
-    lines = sys.stdin.readlines()
-    # Since our input would only be having one line, parse our JSON data from that
-    return json.loads(lines[0])
 
-def saveQueryDetails(queryDetails):
+def saveQueryDetails(requestedSettings):
     # csvPath = os.path.join(os.path.dirname(__file__),"../../tmp/")
     # os.chdir(csvPath)
     # with open('queryDetails.txt','w') as write_file:
     #     json.dump(queryDetails, write_file)
     return
 
-def createQueryUrl(queryDetails):
+def createQueryUrl(requestedSettings):
     # Get the details out of the object
-    aggregator = queryDetails['aggregator']
-    downsample = queryDetails['downsample']
-    rate = queryDetails['metric']
-    tagKey = queryDetails['tagKey']
-    tagValue = queryDetails['tagValue']
-    metric = queryDetails['metric']
-    host = queryDetails['host']
-    port = queryDetails['port']
-    ms = queryDetails['ms']
-    arrays = queryDetails['arrays']
-    tsuids = queryDetails['tsuids']
-    annotations = queryDetails['annotations']
-    startDate = queryDetails['startDate']
-    endDate = queryDetails['endDate']
+    aggregator = requestedSettings['aggregator']
+    downsample = requestedSettings['downsample']
+    tagKey = requestedSettings['tagKey']
+    tagValue = requestedSettings['tagValue']
+    metric = requestedSettings['metric']
+    host = requestedSettings['host']
+    port = requestedSettings['port']
+    ms = requestedSettings['ms']
+    arrays = requestedSettings['arrays']
+    tsuids = requestedSettings['tsuids']
+    annotations = requestedSettings['annotations']
+    startDate = requestedSettings['startDate']
+    endDate = requestedSettings['endDate']
+
+    startDate = dateFormatting(startDate)
+    endDate = dateFormatting(endDate)
 
     url = 'http://' + str(host) + ':' + str(port) + '/api/query?' + 'ms=' + ms + '&arrays=' + arrays + '&show_tsuids=' + tsuids + '&global_annotations=' + annotations + '&start=' + startDate + '&end=' + endDate + '&m=' + aggregator + ':' + downsample + ':' + metric + '{' + tagKey + '=' + tagValue + '}'
    
-    # csvPath = os.path.join(os.path.dirname(__file__),"../../tmp/")
-    # os.chdir(csvPath)
-    # f = open('url.txt','w')
-    # f.write(url)
-    # f.close()
+    csvPath = os.path.join(os.path.dirname(__file__),"../../tmp/")
+    os.chdir(csvPath)
+    f = open('url.txt','w')
+    f.write(url)
+    f.close()
 
     return url
+
+def dateFormatting(date):
+    formattedDate = date.replace('-', '/')
+
+    return formattedDate
 
 def queryDatabase(url):
     data = requests.get(url)
     test = data.text
     return test
 
+
 def writeDataToCSV(queryData):
-    # csvPath = os.path.join(os.path.dirname(__file__),"../../tmp/")
-    # os.chdir(csvPath)
-    # with open('pythonData.csv','w') as write_file:
-    #     json.dump(queryData, write_file)    
+    csvPath = os.path.join(os.path.dirname(__file__),"../../tmp/")
+    os.chdir(csvPath)
+    with open('pythonData.csv','w') as write_file:
+        json.dump(queryData, write_file)    
     return
 
 def extractData(queryData):
@@ -78,22 +81,16 @@ def extractData(queryData):
     f.write(data)
     f.close()
 
-def main():
-    #get our data as an array from read_in()
-    queryDetails = read_in()
+def generateDygraphsData(requestedSettings):
     # Save query details to a text file (used for testing)
-    saveQueryDetails(queryDetails)
+    saveQueryDetails(requestedSettings)
 
-    url = createQueryUrl(queryDetails)
+    url = createQueryUrl(requestedSettings)
 
     queryData = queryDatabase(url)
 
-    writeDataToCSV(queryData)
+    # writeDataToCSV(queryData)
 
     extractData(queryData)
 
-    print(queryDetails)
 
-# Start process
-if __name__ == '__main__':
-    main()
